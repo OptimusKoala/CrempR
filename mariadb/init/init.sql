dfdefa7da98e75821c6e97f71e1afe531c30bfba
Michael Bernard <mi.bernard@ldlc.com>
2025-03-13 13:52:55 +0100

    Add default admin

--- mariadb/init/init.sql
+++ mariadb/init/init.sql
@@ -4,3 +4,6 @@ CREATE TABLE users (
     password VARCHAR(255),
     role ENUM('user','admin') DEFAULT 'user'
 );
+
+INSERT INTO users (username, password, role)
+VALUES ('admin', '$2b$12$u/pK3bGkqIsghnqZtEEm9uh9aGlhx1Cwy0gJje44Vysx0MvxlPpiC', 'admin');
